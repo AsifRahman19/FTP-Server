71b5ae77ba4547c60928568c31f7d57bfb08c21b
AsifRahman19 <57133032+AsifRahman19@users.noreply.github.com>
2019-12-03 14:59:20 +0600

    Create games.php

---
+++ games.php
@@ -0,0 +1,48 @@
+<?php
+include "include/header.php";
+include "include/connect.php";
+include "include/navigation.php"
+?>
+<div class="container">
+    <div class="features text-center">
+      
+		<div class="row">
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/mafia2.jpg" class="card-img-top"  width="288" height="200">
+                        <div class="card-body">
+                            <p class="card-text" style="text-align: center;">Maffia 2</p>
+                            <center>
+                            <a href="games/Mafia II-Black_Box.iso">Download</a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/nfs.jpg" class="card-img-top"  width="288" height="200">
+                        <div class="card-body">
+                            <p class="card-text" style="text-align: center;">NFS Heat</p>
+                            <center>
+                            <a href="#">Download</a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/rdr.jpg" class="card-img-top"  width="288" height="200">
+                        <div class="card-body">
+                            <p class="card-text" style="text-align: center;">Red Dead Redemption 2</p>
+                            <center>
+                            <a href="#">Download</a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+</div>
+                
+
+<?php
+include "include/footer.php";
+?>
