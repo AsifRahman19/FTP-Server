a4b1bffa74c852328a805423efdca3db87e8efe0
AsifRahman19 <57133032+AsifRahman19@users.noreply.github.com>
2019-12-03 15:01:22 +0600

    Create movies.php

---
+++ movies.php
@@ -0,0 +1,46 @@
+<?php
+include "include/header.php";
+include "include/connect.php";
+include "include/navigation.php"
+?>
+<div class="container">
+    <div class="features text-center">
+      
+		<div class="row">
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/avengers.jpg" class="card-img-top"  width="288" height="200">
+                        <div class="card-body">
+                            <p class="card-text" style="text-align: center;">Avengers Endgame</p>
+                            <center>
+                            <a href="F:\movie\London Has Fallen (2016).mp4">Download</a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/johnwick2.jpg" class="card-img-top" width="288" height="200">
+                        <div class="card-body">
+                            <p class="card-text" style="text-align: center;">John Wick 2</p>
+                           <center>
+                            <a href="movies/John Wick Chapter 2 [2017].mkv">Download</a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/12strong.jpg" class="card-img-top" width="288" height="200">
+                        <div class="card-body">
+                            <p class="card-text" style="text-align: center;">12 Strong</p>
+                           <center>
+                            <a href="movies/12 Strong[2018].mp4">Download</a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                
+<?php
+include "include/footer.php";
+?>
